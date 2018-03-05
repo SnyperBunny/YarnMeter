753cb542427de926ac20888a5f0d7b7e9eb4c427
Leonie <leonie.tharratt@gmail.com>
2018-03-04 21:22:46 -0800

    / ‘LED_screen_test_code_V1/LED_screen_test_code_V1.ino’

--- LED_screen_test_code_V1/LED_screen_test_code_V1.ino
+++ LED_screen_test_code_V1/LED_screen_test_code_V1.ino
@@ -1,19 +1,4 @@
-
-/*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-This example is for a 128x32 size display using I2C to communicate
-3 pins are required to interface (2 I2C and one reset)
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-*********************************************************************/
-
-#include <SPI.h>
+//#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -46,11 +31,14 @@ void setup()   {
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("Hello, world!");
+  
   display.setTextColor(BLACK, WHITE); // 'inverted' text
   display.println(3.141592);
+  
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.print("0x"); display.println(0xDEADBEEF, HEX);
+  
   display.display();
   delay(2000);
   display.clearDisplay();
