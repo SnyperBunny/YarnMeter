43055d153818233e4a9b10b7abf7c8407e05200a
Leonie <leonie.tharratt@gmail.com>
2018-03-04 21:39:04 -0800

    / ‘LED_screen_test_code_V1/LED_screen_test_code_V1.ino’

--- LED_screen_test_code_V1/LED_screen_test_code_V1.ino
+++ LED_screen_test_code_V1/LED_screen_test_code_V1.ino
@@ -31,7 +31,7 @@ void setup()   {
   */
   //textsize 3 gives 7 chars across
   //textsize 4 gives 5 chars across
-  float test=5500.3;
+  float test=55000.3;
 
   if (test>=10000){
     display.setTextSize(2);
