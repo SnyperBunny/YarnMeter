ed12d8f91393027a55d8a3c636a6d2e4ec332c30
Leonie <leonie.tharratt@gmail.com>
2018-03-04 21:05:39 -0800

    / ‘LED_screen_test_code_V1/LED_screen_test_code_V1.ino’

--- LED_screen_test_code_V1/LED_screen_test_code_V1.ino
+++ LED_screen_test_code_V1/LED_screen_test_code_V1.ino
@@ -58,17 +58,6 @@ void setup()   {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
   // init done
   
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
-  display.display();
-  delay(2000);
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  // draw a single pixel
- // display.drawPixel(10, 10, WHITE);
   // Show the display buffer on the hardware.
   // NOTE: You _must_ call display after making any drawing commands
   // to make them visible on the display hardware!
