92708a950f17da8b07a30966516f7685b4b8632d
Leonie <leonie.tharratt@gmail.com>
2018-03-04 21:24:08 -0800

    / ‘LED_screen_test_code_V1/LED_screen_test_code_V1.ino’

--- LED_screen_test_code_V1/LED_screen_test_code_V1.ino
+++ LED_screen_test_code_V1/LED_screen_test_code_V1.ino
@@ -9,11 +9,11 @@ Adafruit_SSD1306 display(OLED_RESET);
 //#define NUMFLAKES 10
 //#define XPOS 0
 //#define YPOS 1
-#define DELTAY 2
+//#define DELTAY 2
 
 
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
+//#define LOGO16_GLCD_HEIGHT 16 
+//#define LOGO16_GLCD_WIDTH  16 
 
 
 void setup()   {                
