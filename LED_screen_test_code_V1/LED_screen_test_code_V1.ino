c762ab06a625ba3dfa1e563d0e58e9896a7332fd
Leonie <leonie.tharratt@gmail.com>
2018-03-04 21:25:29 -0800

    / ‘LED_screen_test_code_V1/LED_screen_test_code_V1.ino’

--- LED_screen_test_code_V1/LED_screen_test_code_V1.ino
+++ LED_screen_test_code_V1/LED_screen_test_code_V1.ino
@@ -6,15 +6,8 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-//#define NUMFLAKES 10
-//#define XPOS 0
-//#define YPOS 1
-//#define DELTAY 2
-
-
 //#define LOGO16_GLCD_HEIGHT 16 
 //#define LOGO16_GLCD_WIDTH  16 
-
 
 void setup()   {                
   Serial.begin(9600);
@@ -27,15 +20,16 @@ void setup()   {
   display.clearDisplay();
 
   // text display tests
+  /*
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("Hello, world!");
-  
+  */
   display.setTextColor(BLACK, WHITE); // 'inverted' text
   display.println(3.141592);
   
-  display.setTextSize(2);
+  display.setTextSize(3);
   display.setTextColor(WHITE);
   display.print("0x"); display.println(0xDEADBEEF, HEX);
   
